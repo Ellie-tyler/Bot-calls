d61f9ae3092c1fabb579069d82fa60d9bcc1dfb9
Ellie-tyler <ellietyler794@gmail.com>
2025-05-14 00:23:00 +0530

    Update twilio_call.py

--- twilio_call.py
+++ twilio_call.py
@@ -3,8 +3,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-account_sid = os.getenv("AC0437cc7a19b887a777ad6ddc2e0e40a1")
-auth_token = os.getenv("81e1bfb5ec52421fea308a1eda41edce")
+account_sid = os.getenv("")
+auth_token = os.getenv("")
 client = Client(account_sid, auth_token)
 
 def make_call(to_number, from_number, webhook_url):
